e73919556504fb338f50638401298275e4e46579
lamanaser <Lamazahi40@gmail.com>
2025-06-23 15:42:03 +0300

    moified app.py code

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def summarize():
 
     return jsonify({"sentences": response_data})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
