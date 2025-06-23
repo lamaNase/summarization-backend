58ca7dc3bac0b6546fd8a46bbb052e3331acc5ea
lamanaser <Lamazahi40@gmail.com>
2025-06-23 18:05:39 +0300

    dynamic port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from keybert import KeyBERT
@@ -52,4 +53,5 @@ def summarize():
     return jsonify({"sentences": response_data})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
